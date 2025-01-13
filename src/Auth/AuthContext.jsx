cb7f1150ebbbb2682fb389f7b150063652ba21f5
Raiyan27 <abdullahalraiyan4@gmail.com>
2025-01-14 01:23:24 +0600

    jwt token system implemented

--- src/Auth/AuthContext.jsx
+++ src/Auth/AuthContext.jsx
@@ -1,17 +1,30 @@
 import { createContext, useState, useEffect } from "react";
 import { onAuthStateChanged, getAuth } from "firebase/auth";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const [loading, setLoading] = useState(true);
-
+  const axiosPublic = useAxiosPublic();
   const auth = getAuth();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setCurrentUser(user);
+      if (user) {
+        const userInfo = {
+          email: user.email,
+        };
+        axiosPublic.post("/jwt", userInfo).then((res) => {
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
+          }
+        });
+      } else {
+        localStorage.removeItem("access-token");
+      }
       setLoading(false);
     });
 
